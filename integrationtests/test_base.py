a6970a288d60228c89c5a910117da0fcba096197
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-02 00:47:01 +0700

    Fix capabilities firefox

--- integrationtests/test_base.py
+++ integrationtests/test_base.py
@@ -1,13 +1,16 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
 
 class IntegrationTest(StaticLiveServerTestCase):
 
     def test_something(self):
+        cap = DesiredCapabilities().FIREFOX
+        cap["marionette"] = False
         ff_options = Options()
         ff_options.headless = True
-        browser = webdriver.Firefox(options=ff_options)
+        browser = webdriver.Firefox(options=ff_options, capabilities=cap)
         browser.get(self.live_server_url)
         self.assertEqual(browser.title, "Welcome to AsyncTeam")
