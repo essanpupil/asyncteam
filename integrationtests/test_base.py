afc521ac7f1cc586bb77e38d2a2f235c21e1fb81
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-02 01:15:02 +0700

    Focus testing for windows local dev machine

--- integrationtests/test_base.py
+++ integrationtests/test_base.py
@@ -1,16 +1,13 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
 
 class IntegrationTest(StaticLiveServerTestCase):
 
     def test_something(self):
-        cap = DesiredCapabilities().FIREFOX
-        cap["marionette"] = False
         ff_options = Options()
         ff_options.headless = True
-        browser = webdriver.Firefox(options=ff_options, capabilities=cap, executable_path="/usr/local/bin/geckodriver")
+        browser = webdriver.Firefox(options=ff_options)
         browser.get(self.live_server_url)
         self.assertEqual(browser.title, "Welcome to AsyncTeam")
