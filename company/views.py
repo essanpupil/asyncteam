f291a1c7d6b917ad6421000d9ed5021e53c925bd
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-04 16:48:11 +0700

    Refactor homepage, add link from card to page detail

--- company/views.py
+++ company/views.py
@@ -6,8 +6,17 @@ from company.models import Business, Page
 
 def home(request):
     business = Business.objects.first()
-    return render(request, 'company/home.html', {'business': business})
+    business_pages = Page.objects.filter(business=business, display_in_navbar=True)
+    return render(request, 'company/home.html', {'business': business, 'business_pages': business_pages})
 
 
 class PageView(DetailView):
     model = Page
+
+    def get_context_data(self, **kwargs):
+        context = super(PageView, self).get_context_data(**kwargs)
+        page = self.get_object()
+        business_pages = Page.objects.filter(business=page.business, display_in_navbar=True)
+        context['business'] = page.business
+        context['business_pages'] = business_pages
+        return context
