6e1c644bae7d5ac193a1d7db3ea5e807f3eaeb3a
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-04 11:26:16 +0700

    refactor integration test to close browser after each test

--- integrationtests/test_base.py
+++ integrationtests/test_base.py
@@ -1,3 +1,4 @@
+import pytest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -5,9 +6,16 @@ from selenium.webdriver.firefox.options import Options
 
 class IntegrationTest(StaticLiveServerTestCase):
 
-    def test_something(self):
+    def setUp(self) -> None:
         ff_options = Options()
-        ff_options.headless = True
-        browser = webdriver.Firefox(options=ff_options)
-        browser.get(self.live_server_url)
-        self.assertEqual(browser.title, "Welcome to AsyncTeam")
+        ff_options.headless = False
+        self.browser = webdriver.Firefox(options=ff_options)
+        self.browser.get(self.live_server_url)
+
+    def tearDown(self) -> None:
+        self.browser.close()
+
+    @pytest.mark.django_db(transaction=True)
+    def test_something(self):
+        self.browser.get(self.live_server_url)
+        self.assertEqual(self.browser.title, "Welcome to AsyncTeam")
