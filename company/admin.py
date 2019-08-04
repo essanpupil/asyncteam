4a61fe01c528787cf16f27cda6bc024096d8e389
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-04 15:42:35 +0700

    Make homepage content configurable

--- company/admin.py
+++ company/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Business, Card, Page
+from .models import Business, Card, Page, Testimony, Contact
 
 admin.site.register(Business)
 admin.site.register(Card)
+admin.site.register(Testimony)
+admin.site.register(Contact)
 
 
 class PageAdmin(admin.ModelAdmin):
