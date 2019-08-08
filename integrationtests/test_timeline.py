8b40b88f1dc9c8caeb4dd39c2a8973ec542ca625
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-08 08:28:00 +0700

    [WIP] Initiate timeline creation

--- integrationtests/test_timeline.py
+++ integrationtests/test_timeline.py
@@ -2,6 +2,13 @@ from integrationtests.base_testing import IntegrationTest
 
 
 class TimelineTest(IntegrationTest):
+    def go_to_timeline_dashboard(self):
+        self.create_business()
+        self.create_user('jojo', 'badminton')
+        self.logging_in_user(self.user, 'badminton')
+
+        timeline_nav = self.browser.find_element_by_link_text('Timeline')
+        timeline_nav.click()
 
     def test_go_to_timeline_dashboard(self):
         self.create_business()
@@ -11,3 +18,9 @@ class TimelineTest(IntegrationTest):
         timeline_nav = self.browser.find_element_by_link_text('Timeline')
         timeline_nav.click()
         self.assertEqual(self.browser.title, 'Timeline Dashboard')
+
+    def test_create_main_timeline(self):
+        self.go_to_timeline_dashboard()
+        create_timeline_menu = self.browser.find_element_by_id('create-main-timeline')
+        create_timeline_menu.click()
+        self.assertEqual("Create main timeline", self.browser.title)
