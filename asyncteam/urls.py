6205552d36db9508c80c1c7dcaddf5c564f37d2b
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-11 22:57:55 +0700

    Fix styleing navbar, highlight current page

--- asyncteam/urls.py
+++ asyncteam/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
