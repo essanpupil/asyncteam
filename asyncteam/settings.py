9d772fa661abfe81185c487b0911457cf2f4b07f
Ikhsan N. Rosyidin <jakethitam1985@gmail.com>
2019-08-12 01:35:29 +0700

    styling dashboard

--- asyncteam/settings.py
+++ asyncteam/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'asyncteam/static/'),
+]
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
